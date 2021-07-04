755b942c6df4e74e54b4015b2012acb9dcb4a977
Thiago <astratone@gmail.com>
2021-07-04 00:19:02 -0300

    Including calculate button codes.

--- app/src/main/java/com/example/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_imc/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.calculadora_imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.linear_main)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        et_altura.doAfterTextChanged { text ->
+            //Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        et_peso.doOnTextChanged { text, _, _, _->
+            //tv_titulo.text = text
+        }
+        bt_calculo.setOnClickListener {
+            calcularIMC(et_peso.text.toString(), et_altura.text.toString())
+        }
+
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            tv_titulo.text = "Seu IMC é:\n%.2f".format(imc)
+        }
     }
 }
